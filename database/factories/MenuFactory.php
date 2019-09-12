bc32801659b5d89a92837f5814da8dfdd9d77540
Sunkanmi Olukitibi <sunkanmikitibi@gmail.com>
2019-09-12 11:52:14 +0100

    second phase complete

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Menu::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(2, true),
+        'price'=>$faker->numberBetween(99, 99999),
+    ];
+});
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+         \App\User::create([
+             'name' =>'Olamide Olukitibi',
+             'email' => 'sunkitibi@gmail.com',
+             'password'=> bcrypt('sunkanmi'),
+         ]);
     }
 }
